33e64059b8e3180b2b289a42ac0e00c04418bed0
stephan lalka <slalka@gmail.com>
2017-11-04 23:42:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist(playlist,artistName,songTitle){
     playlist[artistName] = songTitle;
     return playlist;
 }
+function removeFromPlaylist(p,a,s){
+  delete p[a];
+  return p;
+}
